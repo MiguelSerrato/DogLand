ca763ce133e8ac0eeef1cf6f9760e06ca8498133
Miguel Serrato <pau.jmsm@gmail.com>
2025-06-05 15:31:10 -0600

    Initial Commit

---
+++ DogLand/DogLandApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogLandApp.swift
+//  DogLand
+//
+//  Created by Jose Miguel Serrato Moreno on 05/06/25.
+//
+
+import SwiftUI
+
+@main
+struct DogLandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
